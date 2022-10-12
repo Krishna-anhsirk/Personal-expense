a4a3a932ec9173e6d9426251ddc1768071087f5a
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-10-12 16:09:08 +0530

    show output

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const btnAddExpense = document.getElementById("btn-add");
 const inputAmount = document.querySelector("#input-amount");
+const headingElement = document.querySelector(".total-expense");
 
 let totalExpense = 0;
+headingElement.textContent = totalExpense;
+
 function addExpense() {
   const expense = Number(inputAmount.value);
   totalExpense = totalExpense + expense;
+  headingElement.textContent = totalExpense;
 }
 
 btnAddExpense.addEventListener("click", addExpense, false);
