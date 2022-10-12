5a37b5a520c91ec85124dc2373e1134e215b76db
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-10-12 15:56:20 +0530

    totalExpense implemented

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-let counter = -1;
-const clickNumber = document.getElementById("click-number");
-clickNumber.addEventListener("click", () => {
-  counter++;
-  console.log(counter);
-});
+const btnAddExpense = document.getElementById("btn-add");
+const inputAmount = document.querySelector("#input-amount");
+
+let totalExpense = 0;
+function addExpense() {
+  const expense = Number(inputAmount.value);
+  totalExpense = totalExpense + expense;
+}
+
+btnAddExpense.addEventListener("click", addExpense, false);
