31cedd388f91fc8cd2b696c5d19e7dd21cb9b89b
Krishna-anhsirk <Krishnatejajinka@gmail.com>
2022-10-12 16:59:45 +0530

    show all expenses on screen

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
 const btnAddExpense = document.getElementById("btn-add");
-const inputAmount = document.querySelector("#input-amount");
+const inputAmount = document.querySelector("#expense-amount");
 const headingElement = document.querySelector(".total-expense");
+const expenseName = document.querySelector("#expense-name");
+const expenseTable = document.querySelector("#expense-table");
 
 let totalExpense = 0;
 headingElement.textContent = totalExpense;
 
+const allExpenses = [];
+
 function addExpense() {
-  const expense = Number(inputAmount.value);
-  totalExpense = totalExpense + expense;
+  const textAmount = Number(inputAmount.value);
+  const textDesc = expenseName.value;
+  totalExpense = totalExpense + textAmount;
   headingElement.textContent = totalExpense;
+  allExpenses.push({ expenseAmount: textAmount, expenseName: textDesc });
+
+  const allExpensesHTML = allExpenses.map((expense) => {
+    return `<div>${textDesc}::${textAmount}</div>`;
+  });
+
+  const joinedAllExpensesHTML = allExpensesHTML.join(" ");
+  expenseTable.innerHTML = joinedAllExpensesHTML;
 }
 
 btnAddExpense.addEventListener("click", addExpense, false);
